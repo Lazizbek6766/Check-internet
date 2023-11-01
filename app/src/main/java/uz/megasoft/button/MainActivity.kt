0a75e00a95550a5f916b95c1443e15292ce98f4c
Lazizbek6766 <turayevlazizbek6766@gmail.com>
2023-11-01 11:47:04 +0500

    inital2

--- app/src/main/java/uz/megasoft/button/MainActivity.kt
+++ app/src/main/java/uz/megasoft/button/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.IntentFilter
 import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.view.View
 import android.widget.Button
@@ -17,32 +18,54 @@ class MainActivity : AppCompatActivity() {
         CheckInternet()
     }
 
+    private val networkMonitor = NetworkMonitorUtil(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        checkInternet.observe(this, Observer { isInternetEnabled ->
-            if (isInternetEnabled) {
-                // Internet ulanish mavjud
-                Toast.makeText(this, "Internet ulanish bor", Toast.LENGTH_SHORT).show()
-            } else {
-                // Internet ulanishi yo'q
-                Toast.makeText(this, "Internet ulanish yo'q", Toast.LENGTH_SHORT).show()
+        networkMonitor.result = { isAvailable, type ->
+            runOnUiThread {
+                when (isAvailable) {
+                    true -> {
+                        when (type) {
+                            ConnectionType.Wifi -> {
+                                Log.d("TAG", "onCreate: wifi")
+
+                            }
+                            ConnectionType.Cellular -> {
+                                Log.d("TAG", "onCreate: internet")
+                            }
+                            else -> { }
+                        }
+                    }
+                    false -> {
+                        Log.d("TAG", "onCreate: yo'q")
+                    }
+                }
             }
-        })
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        networkMonitor.register()
     }
 
     override fun onStart() {
         super.onStart()
         // BroadcastReceiver'ni aktivatsiyalash
-        val intentFilter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
-        registerReceiver(checkInternet.internetSwitchStateReceiver, intentFilter)
+//        val intentFilter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+//        registerReceiver(checkInternet.internetSwitchStateReceiver, intentFilter)
     }
 
     override fun onStop() {
         super.onStop()
+
+        networkMonitor.unregister()
+
         // BroadcastReceiver'ni o'chirish
-        unregisterReceiver(checkInternet.internetSwitchStateReceiver)
+//        unregisterReceiver(checkInternet.internetSwitchStateReceiver)
     }
 }
